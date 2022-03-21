8db990baf6c88938e05e74feeabeb3110ec5a6bb
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-03-21 11:08:19 +0800

    main

--- src/app/together/together_gui.c
+++ src/app/together/together_gui.c
@@ -81,10 +81,6 @@ void together_gui_init(void) // style init
     lv_style_set_bg_color(&default_style, LV_STATE_FOCUSED | LV_STATE_PRESSED, lv_color_hex(0xf88));
     lv_obj_add_style(together_scr, LV_BTN_PART_MAIN, &default_style);
 
-  	// lv_style_init(&name_style);
-    // lv_style_set_text_opa(&name_style, LV_STATE_DEFAULT, LV_OPA_COVER);
-    // lv_style_set_text_color(&name_style, LV_STATE_DEFAULT, LV_COLOR_WHITE);
-    // lv_style_set_text_font(&name_style, LV_STATE_DEFAULT, &lcNameFont);
 
 	lv_style_init(&time_style);
     lv_style_set_text_opa(&time_style, LV_STATE_DEFAULT, LV_OPA_COVER);
@@ -109,33 +105,6 @@ void UIInit(){ // ui init
         return;
     else
         lv_obj_clean(lv_scr_act());
-    
-	// la_name = lv_label_create(together_scr, NULL);
-    // lv_obj_add_style(la_name, LV_LABEL_PART_MAIN, &name_style);
-    // lv_label_set_recolor(la_name , true);
-    // lv_label_set_text_fmt(la_name , "#4246dd LC#   #ffae95 ZY#");//#ffae95 ZY#     #4246dd LC#
-	// // lv_obj_align(la_name, NULL, LV_ALIGN_IN_TOP_LEFT, 0, LV_DPI / 20);
-    // lv_obj_set_pos(la_name, 40, 20);
-
-    // img_flowers_5= lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_flowers_5, la_name, LV_ALIGN_OUT_BOTTOM_MID, 0, -5);
-    // lv_img_set_src(img_flowers_5, &flowers_5);
-
-    // img_note= lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_note, img_flowers_5, LV_ALIGN_OUT_BOTTOM_MID, -65, -5);
-    // lv_img_set_src(img_note, &note);
-
-	// la_day = lv_label_create(together_scr, NULL);
-	// lv_obj_add_style(la_day, LV_LABEL_PART_MAIN, &time_style);
-    // lv_label_set_recolor(la_day , true);
-    // lv_label_set_text_fmt(la_day , "已营业%04d天", 1439);
-	// lv_obj_align(la_day, img_note, LV_ALIGN_IN_LEFT_MID, 35, 2);
-
-	// la_sec = lv_label_create(together_scr, NULL);
-	// lv_obj_add_style(la_sec, LV_LABEL_PART_MAIN, &time_style);
-    // lv_label_set_recolor(la_sec , true);
-    // lv_label_set_text_fmt(la_sec , "%10d秒", 123904400);
-	// lv_obj_align(la_sec, la_day, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
 
     clockLabel_1 = lv_label_create(together_scr, NULL);
     lv_obj_add_style(clockLabel_1, LV_LABEL_PART_MAIN, &numberBig_style);
@@ -149,50 +118,7 @@ void UIInit(){ // ui init
     lv_obj_align(clockLabel_2, clockLabel_1, LV_ALIGN_OUT_RIGHT_MID, 5, 0);
 
     
-  
-// 	img_heart = lv_img_create(together_scr, NULL);
-//    // lv_img_set_src(img_heart, heart1.frames[0]);
-// 	lv_obj_align(img_heart, la_name, LV_ALIGN_IN_TOP_MID, 0, LV_DPI / 20);
-//     lv_img_set_src(img_heart, &transparent);
 
-    // img_bottom_left= lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_bottom_left, together_scr, LV_ALIGN_IN_BOTTOM_LEFT, 0, -25);
-
-
-    // img_love_7= lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_love_7, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 0);
-
-    // img_love= lv_img_create(together_scr, NULL);
-	// // lv_obj_align(img_love, NULL, LV_ALIGN_IN_TOP_LEFT, 10, -8);
-    // // lv_obj_set_pos(img_love, 190, 90);
-	// lv_obj_align(img_love, la_name, LV_ALIGN_CENTER, 16, 0);
-
-    // img_rainbow = lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_rainbow, la_name, LV_ALIGN_OUT_TOP_RIGHT, 60, 25);
-    // lv_img_set_src(img_rainbow, &rainbow);
-
-    // // img_top_left= lv_img_create(together_scr, NULL);
-	// // lv_obj_align(img_top_left, together_scr, LV_ALIGN_IN_TOP_LEFT, 0, 0);
-
-    // // img_top_right= lv_img_create(together_scr, NULL);
-	// // lv_obj_align(img_top_right, together_scr, LV_ALIGN_IN_TOP_RIGHT, 0, -25);
-
-    // img_we = lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_we, together_scr, LV_ALIGN_IN_BOTTOM_RIGHT, 10, -50);
-    // lv_img_set_src(img_we, our_photo[our_photo_num-1]);
-    
-    // img_photo_frame= lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_photo_frame, img_we, LV_ALIGN_CENTER, 2, -15);
-    // lv_img_set_src(img_photo_frame, &photo_frame);
-
-    // img_twinkle = lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_twinkle, img_note, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
-
-    // img_cute_emoji = lv_img_create(together_scr, NULL);
-	// lv_obj_align(img_cute_emoji, img_photo_frame, LV_ALIGN_OUT_LEFT_MID, -10, -20);
-
-  //  lv_scr_load_anim(together_scr, LV_SCR_LOAD_ANIM_NONE, 300, 300, false);
-     // or   
     lv_scr_load(together_scr);
 }
 
@@ -212,22 +138,9 @@ void stopGif(lv_obj_t* img){
     lv_img_set_src(img, &transparent);
 }
 
-void display_time_ani(long long networkTime, uint32_t t){
-    // static unsigned int last_tick = 0;
-    // uint32_t now = lv_tick_get();
-    // if(now - last_tick >= 1000){
-    //     long long totalSec = (networkTime + lv_tick_get() - t - fall_in_love_time_stamp)/1000;
-    //     lv_label_set_text_fmt(la_sec , "%10d秒", totalSec);
-    //     last_tick = now;
-    //     static unsigned int totalDay = 1461;
-    //     unsigned int day = totalSec/86400;
-    //     if(totalDay != day){
-    //          totalDay = day;
-    //          lv_label_set_text_fmt(la_day , "已营业%04d天", totalDay);
-    //     }
-    // }t
-    // lv_label_set_text_fmt(clockLabel_1, "%02d#ffa500 %02d#", t.hour, t.minute);
-    // lv_label_set_text_fmt(clockLabel_2, "%02d", t.second);
+void display_time_ani(long long networkTime, uint32_t t)
+{
+
 }
 
 void display_us(const char *file_name)
@@ -264,15 +177,6 @@ void display_cute_emoji()
 
 void display_together(long long networkTime,struct TimeStr t) {
     UIInit();
-    // displayGif(img_bottom_left, &gif_obj_bottom_left);
-    // // displayGif(img_top_left, &gif_obj_top_left);
-    // // displayGif(img_top_right, &gif_obj_top_right);
-    // displayGif(img_love_7, &gif_obj_love_7);
-    // displayGif(img_love, &gif_obj_love);
-    // displayGif(img_twinkle, &gif_obj_twinkle);
-   // display_time_ani(networkTime, t);
-    // display_cute_emoji();
-    // display_our_photo();
     lv_label_set_text_fmt(clockLabel_1, "#ffa500 %02d#", t.minute);//#ffa500 
     lv_label_set_text_fmt(clockLabel_2, "%02d", t.second);
 }
